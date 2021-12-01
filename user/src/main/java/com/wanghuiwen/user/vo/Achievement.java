3426d9af4b6e9956cecef300ab9a223b4e325442
huiwenwang <123456>
2021-12-01 13:51:36 +0800

    问题修复

--- user/src/main/java/com/wanghuiwen/user/vo/Achievement.java
+++ user/src/main/java/com/wanghuiwen/user/vo/Achievement.java
@@ -1,122 +1,131 @@
 package com.wanghuiwen.user.vo;
 
 public class Achievement {
-    private int referralSend;
-    private int referralReceived;
-    private int thankYouNoteSend;
-    private int thankYouNoteSum;
-    private int thankYouNoteReceived;
-    private int prevReferralSend;
-    private int prevReferralReceived;
-    private int prevThankYouNoteSend;
-    private int prevThankYouNoteReceived;
+    private Long id;
+    private Integer referralSend;
+    private Integer referralReceived;
+    private Integer thankYouNoteSend;
+    private Integer thankYouNoteSum;
+    private Integer thankYouNoteReceived;
+    private Integer prevReferralSend;
+    private Integer prevReferralReceived;
+    private Integer prevThankYouNoteSend;
+    private Integer prevThankYouNoteReceived;
 
-    private int referralSendCount;
-    private int referralReceivedCount;
-    private int thankYouNoteSendCount;
-    private int thankYouNoteReceivedCount;
+    private Integer referralSendCount;
+    private Integer referralReceivedCount;
+    private Integer thankYouNoteSendCount;
+    private Integer thankYouNoteReceivedCount;
 
-    public int getReferralSend() {
+    public Integer getReferralSend() {
         return referralSend;
     }
 
-    public void setReferralSend(int referralSend) {
+    public void setReferralSend(Integer referralSend) {
         this.referralSend = referralSend;
     }
 
-    public int getReferralReceived() {
+    public Integer getReferralReceived() {
         return referralReceived;
     }
 
-    public void setReferralReceived(int referralReceived) {
+    public void setReferralReceived(Integer referralReceived) {
         this.referralReceived = referralReceived;
     }
 
-    public int getThankYouNoteSend() {
+    public Integer getThankYouNoteSend() {
         return thankYouNoteSend;
     }
 
-    public void setThankYouNoteSend(int thankYouNoteSend) {
+    public void setThankYouNoteSend(Integer thankYouNoteSend) {
         this.thankYouNoteSend = thankYouNoteSend;
     }
 
-    public int getThankYouNoteReceived() {
-        return thankYouNoteReceived;
-    }
-
-    public void setThankYouNoteReceived(int thankYouNoteReceived) {
-        this.thankYouNoteReceived = thankYouNoteReceived;
-    }
-
-    public int getPrevReferralSend() {
-        return prevReferralSend;
-    }
-
-    public void setPrevReferralSend(int prevReferralSend) {
-        this.prevReferralSend = prevReferralSend;
-    }
-
-    public int getPrevReferralReceived() {
-        return prevReferralReceived;
-    }
-
-    public void setPrevReferralReceived(int prevReferralReceived) {
-        this.prevReferralReceived = prevReferralReceived;
-    }
-
-    public int getPrevThankYouNoteSend() {
-        return prevThankYouNoteSend;
-    }
-
-    public void setPrevThankYouNoteSend(int prevThankYouNoteSend) {
-        this.prevThankYouNoteSend = prevThankYouNoteSend;
-    }
-
-    public int getPrevThankYouNoteReceived() {
-        return prevThankYouNoteReceived;
-    }
-
-    public void setPrevThankYouNoteReceived(int prevThankYouNoteReceived) {
-        this.prevThankYouNoteReceived = prevThankYouNoteReceived;
-    }
-
-    public int getReferralSendCount() {
-        return referralSendCount;
-    }
-
-    public void setReferralSendCount(int referralSendCount) {
-        this.referralSendCount = referralSendCount;
-    }
-
-    public int getReferralReceivedCount() {
-        return referralReceivedCount;
-    }
-
-    public void setReferralReceivedCount(int referralReceivedCount) {
-        this.referralReceivedCount = referralReceivedCount;
-    }
-
-    public int getThankYouNoteSendCount() {
-        return thankYouNoteSendCount;
-    }
-
-    public void setThankYouNoteSendCount(int thankYouNoteSendCount) {
-        this.thankYouNoteSendCount = thankYouNoteSendCount;
-    }
-
-    public int getThankYouNoteReceivedCount() {
-        return thankYouNoteReceivedCount;
-    }
-
-    public void setThankYouNoteReceivedCount(int thankYouNoteReceivedCount) {
-        this.thankYouNoteReceivedCount = thankYouNoteReceivedCount;
-    }
-
-    public int getThankYouNoteSum() {
+    public Integer getThankYouNoteSum() {
         return thankYouNoteSum;
     }
 
-    public void setThankYouNoteSum(int thankYouNoteSum) {
+    public void setThankYouNoteSum(Integer thankYouNoteSum) {
         this.thankYouNoteSum = thankYouNoteSum;
+    }
+
+    public Integer getThankYouNoteReceived() {
+        return thankYouNoteReceived;
+    }
+
+    public void setThankYouNoteReceived(Integer thankYouNoteReceived) {
+        this.thankYouNoteReceived = thankYouNoteReceived;
+    }
+
+    public Integer getPrevReferralSend() {
+        return prevReferralSend;
+    }
+
+    public void setPrevReferralSend(Integer prevReferralSend) {
+        this.prevReferralSend = prevReferralSend;
+    }
+
+    public Integer getPrevReferralReceived() {
+        return prevReferralReceived;
+    }
+
+    public void setPrevReferralReceived(Integer prevReferralReceived) {
+        this.prevReferralReceived = prevReferralReceived;
+    }
+
+    public Integer getPrevThankYouNoteSend() {
+        return prevThankYouNoteSend;
+    }
+
+    public void setPrevThankYouNoteSend(Integer prevThankYouNoteSend) {
+        this.prevThankYouNoteSend = prevThankYouNoteSend;
+    }
+
+    public Integer getPrevThankYouNoteReceived() {
+        return prevThankYouNoteReceived;
+    }
+
+    public void setPrevThankYouNoteReceived(Integer prevThankYouNoteReceived) {
+        this.prevThankYouNoteReceived = prevThankYouNoteReceived;
+    }
+
+    public Integer getReferralSendCount() {
+        return referralSendCount;
+    }
+
+    public void setReferralSendCount(Integer referralSendCount) {
+        this.referralSendCount = referralSendCount;
+    }
+
+    public Integer getReferralReceivedCount() {
+        return referralReceivedCount;
+    }
+
+    public void setReferralReceivedCount(Integer referralReceivedCount) {
+        this.referralReceivedCount = referralReceivedCount;
+    }
+
+    public Integer getThankYouNoteSendCount() {
+        return thankYouNoteSendCount;
+    }
+
+    public void setThankYouNoteSendCount(Integer thankYouNoteSendCount) {
+        this.thankYouNoteSendCount = thankYouNoteSendCount;
+    }
+
+    public Integer getThankYouNoteReceivedCount() {
+        return thankYouNoteReceivedCount;
+    }
+
+    public void setThankYouNoteReceivedCount(Integer thankYouNoteReceivedCount) {
+        this.thankYouNoteReceivedCount = thankYouNoteReceivedCount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
